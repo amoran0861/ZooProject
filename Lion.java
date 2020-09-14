a9ab95bb3a44834879e60ddd722e7f22f2c5b8e9
Aidan <amoran0861@myduneland.org>
2020-09-14 08:48:15 -0500

    Initial sharing of project

---
+++ Lion.java
@@ -0,0 +1,10 @@
+public class Lion
+{
+    private int age;
+    
+    public void roar()
+    {
+        System.out.println("Roar!");
+    }
+
+}
---
+++ ZooProgram.java
@@ -0,0 +1,11 @@
+
+public class ZooProgram
+{
+
+    public static void main(String [] args)
+    {
+       Lion leo = new Lion();
+       leo.roar();
+    }
+
+}
